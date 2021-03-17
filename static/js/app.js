fafb11fbefd4d5f8b02d9713f8b33addf8ff2e76
MoSeale <57756845+MoSeale@users.noreply.github.com>
2021-03-17 01:06:52 -0400

    Updated app.js
    Populate Demographic Info using sample data

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,37 @@
 //declare variables
 
 //create metadata function
+d3.json("data/samples.json").then((incomingData) => {
+    //console.log(incomingData)
+    var metadata = incomingData.metadata;
+    //console.log(metadata)
+    //first id for testing
+    //console.log(metadata[0].id)
+    user_selection = metadata[0].id;
+    console.log(user_selection);
+
+
+    ///FIlter metadata on a given id assigned to user_selection
+    var selected_metadata = metadata.filter(row => row.id == user_selection);
+    console.log(selected_metadata);
+
+    ///Populate Demographic Info section of page
+
+    //select panel id "sample-metadata"
+    var demog_info = d3.select("#sample-metadata");
+
+    //refresh info 
+
+    demog_info.html("");
+
+    //add data 
+    Object.entries(selected_metadata[0]).forEach(([key, value])=> {
+        //console.log([value])
+        demog_info.append("h6").text(`${key.toUpperCase()} : ${value}`);
+        
+    });
+}
+)
 
 ///build charts function
 
@@ -15,6 +46,8 @@
 
 ////init function
 
+// on option changes
 
 
 ///call init function
+//init();
