bc38c30999644f317ea9147afbcd90faca72d5a7
MoSeale <57756845+MoSeale@users.noreply.github.com>
2021-03-17 00:28:32 -0400

    Update app.js
    app structure mapped out

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+//declare variables
+
+//create metadata function
+
+///build charts function
+
+    /// call data to populate chart
+
+    ///bar chart
+
+    ///bubble chart
+
+
+/// event handler for the drop down changes
+
+////init function
+
+
+
+///call init function
